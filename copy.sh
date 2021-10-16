4e4f30353580c84be4d8a446228cf0ec5ac983f1
Barne Kleinen <kleinen@infrastructure.de>
2021-10-16 18:16:45 +0000

    add original firewall config

--- copy.sh
+++ copy.sh
@@ -1,3 +1,7 @@
 sudo cp /etc/sudoers etc/sudoers
 cp -r /etc/nginx nginx/save/
 tree /etc/nginx/ > nginx/save/tree
+cp /etc/firewall.conf etc/
+sudo cp /root/firewall.sh firewall
+sudo chown kleinen:kleinen firewall/firewall.sh
+

efc577bc039a93c5888b80148783794a40bacfd6
Barne Kleinen <kleinen@infrastructure.de>
2021-10-16 18:03:02 +0000

    nginx with copy of new configuration

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 sudo cp /etc/sudoers etc/sudoers
-tree /etc/nginx/ > nginx/tree
+cp -r /etc/nginx nginx/save/
+tree /etc/nginx/ > nginx/save/tree
 sudo cp /etc/nginx/sites-available/infrastructure nginx/sites-available-infrastructure
